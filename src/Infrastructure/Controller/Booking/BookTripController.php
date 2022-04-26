a1e9b9b476de3aed1e97b6172af2a9bd144fb64b
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-26 09:02:47 +0300

    setup annotation

---
+++ src/Infrastructure/Controller/Booking/BookTripController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Infrastructure\Controller\Booking;
+
+use Infrastructure\Common\ActionController;
+use Symfony\Component\HttpFoundation\Request;
+
+class BookTripController {
+
+    public function __construct(
+        private ActionController $actionController
+    )
+    {
+        
+    }
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
