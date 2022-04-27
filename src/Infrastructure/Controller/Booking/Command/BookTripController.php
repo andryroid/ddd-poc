b3d802646ece95d8a6d0825cb2007cb79c344c1e
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-27 15:30:50 +0300

    implement response interface

--- src/Infrastructure/Controller/Booking/Command/BookTripController.php
+++ src/Infrastructure/Controller/Booking/Command/BookTripController.php
@@ -4,11 +4,22 @@ namespace Infrastructure\Controller\Booking\Command;
 
 use Infrastructure\Common\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class BookTripController extends AbstractController
 {
-
-    public function __invoke(Request $request)
+    public function __construct(
+        private
+    )
     {
+        
+    }
+    #[Route('/booking', methods: ['POST'], name: "api_booking_create")]
+    public function bookTripAction(Request $request) {
+        return $this->responseManager->success(
+            [
+                'status' => 200,
+                'payload' => "Success"
+            ]);
     }
 }
