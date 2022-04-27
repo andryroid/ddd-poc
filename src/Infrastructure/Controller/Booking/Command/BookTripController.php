eeff3a7105d37f51d3409e880ef0d3b78eb66f59
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-27 15:43:11 +0300

    setup class controller

--- src/Infrastructure/Controller/Booking/Command/BookTripController.php
+++ src/Infrastructure/Controller/Booking/Command/BookTripController.php
@@ -3,17 +3,21 @@
 namespace Infrastructure\Controller\Booking\Command;
 
 use Infrastructure\Common\AbstractController;
+use Infrastructure\Controller\Booking\Response\BookingResponseManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class BookTripController extends AbstractController
 {
-    public function __construct(
-        private
+    public function __construct
+    (
+        private BookingResponseManager $bookingResponseManager
     )
     {
-        
+        parent::__construct($this->bookingResponseManager);
     }
+
+    
     #[Route('/booking', methods: ['POST'], name: "api_booking_create")]
     public function bookTripAction(Request $request) {
         return $this->responseManager->success(
---
+++ src/Infrastructure/Controller/Booking/Response/BookingResponseManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Infrastructure\Controller\Booking\Response;
+
+use Infrastructure\Common\Response\ResponseManagerInterface;
+use JsonSerializable;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class BookingResponseManager implements ResponseManagerInterface {
+
+    public function success(JsonSerializable|array $data): JsonResponse {
+        return new JsonResponse(
+            [
+                'status' => 200,
+                'payload' => $data
+            ],
+            200);
+    }
+
+    public function error(JsonSerializable|array $data): JsonResponse {
+        return new JsonResponse(
+            [
+                'status' => 500,
+                'payload' => $data
+            ],
+            200);
+    }
+
+}
