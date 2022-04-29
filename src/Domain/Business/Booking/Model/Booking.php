32a14ec9ce9805b4ab3d9f8877aba31c34968d86
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-29 08:29:20 +0300

    refactor invalid date

--- src/Domain/Business/Booking/Model/Booking.php
+++ src/Domain/Business/Booking/Model/Booking.php
@@ -3,8 +3,8 @@
 namespace Domain\Business\Booking\Model;
 
 use Domain\Business\Booking\Collection\ContactsInterface;
-use Domain\Business\Booking\Exception\BookingInformationIncorrectException;
 use Domain\Business\Booking\Exception\EmptyContactsException;
+use Domain\Business\Booking\Exception\InvalidBoookingDateException;
 use Domain\Business\Booking\Exception\InvalidDepartureException;
 use Domain\Business\Booking\Model\Properties\Location;
 use Domain\Business\Booking\Model\Properties\Person;
@@ -35,9 +35,9 @@ final class Booking extends AggregateRoot
         if ($departure->isEqual($destination)) {
             throw new InvalidDepartureException("Destination is equal to Departure");
         }
-        //do not understand
+        //Cannot book a trip in the past
         if (new \DateTime() > $departureTime) {
-            throw new BookingInformationIncorrectException("Destination is equal to Departure");
+            throw new InvalidBoookingDateException("Destination is equal to Departure");
         }
 
         $booking = new self(
