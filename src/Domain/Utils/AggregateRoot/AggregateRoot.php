799e54a24b8afc8efc0a155d332fb4f95216bc0e
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-29 09:21:43 +0300

    update ar

--- src/Domain/Utils/AggregateRoot/AggregateRoot.php
+++ src/Domain/Utils/AggregateRoot/AggregateRoot.php
@@ -4,4 +4,7 @@ namespace Domain\Utils\AggregateRoot;
 
 class AggregateRoot {
     protected array $events;
+    protected function addEvent(object $event) : mixed {
+        $this->events[] = $event;
+    }
 }
