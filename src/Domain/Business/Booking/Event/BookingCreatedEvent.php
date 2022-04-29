b637b0afca03c0dd827be232e24f562a082a3d49
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-29 12:03:58 +0300

    update identifier

--- src/Domain/Business/Booking/Event/BookingCreatedEvent.php
+++ src/Domain/Business/Booking/Event/BookingCreatedEvent.php
@@ -3,5 +3,10 @@
 namespace Domain\Business\Booking\Event;
 
 final class BookingCreatedEvent {
-    
+    public function __construct(
+        private readonly string $identifierId
+    )
+    {
+        
+    }
 }
