73373a8feac0ca39e67cfd6cddf7258e6e5aa050
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-04-29 16:21:49 +0300

    create commande

--- src/Application/Booking/CreateBooking/Command/CreateBookingCommand.php
+++ src/Application/Booking/CreateBooking/Command/CreateBookingCommand.php
@@ -2,7 +2,23 @@
 
 namespace Application\Booking\CreateBooking\Command;
 
+use Domain\Business\Booking\Collection\ContactsInterface;
+use Domain\Business\Booking\Model\Booking;
+use Domain\Business\Booking\Model\Properties\Location;
+use Domain\Business\Booking\Model\Properties\Person;
+use Domain\Utils\Identifier\IndentifierInterface;
+
 final class CreateBookingCommand
 {
-
+    public function __construct(
+        public readonly Person $person,
+        public readonly ContactsInterface $contacts,
+        public readonly Location $departure,
+        public readonly Location $destination,
+        public readonly \DateTime $departureTime
+    )
+    {
+        
+    }
+    
 }
