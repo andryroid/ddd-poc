e6e861867d2c87275690c712824786a2183212a4
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-05-30 15:12:50 +0300

    create entity

---
+++ src/Infrastructure/Entity/Booking/Booking.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Infrastructure\Entity\Booking;
+
+use Doctrine\ORM\Mapping as ORM;
+use Infrastructure\Repository\Booking\BookingRepository;
+
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+class Booking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $uuid;
+
+    #[ORM\Column(type: 'json')]
+    private $person = [];
+
+    #[ORM\Column(type: 'json')]
+    private $contacts = [];
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $departure;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $destination;
+
+    #[ORM\Column(type: 'datetime')]
+    private $departure_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getPerson(): ?array
+    {
+        return $this->person;
+    }
+
+    public function setPerson(array $person): self
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    public function getContacts(): ?array
+    {
+        return $this->contacts;
+    }
+
+    public function setContacts(array $contacts): self
+    {
+        $this->contacts = $contacts;
+
+        return $this;
+    }
+
+    public function getDeparture(): ?string
+    {
+        return $this->departure;
+    }
+
+    public function setDeparture(string $departure): self
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getDepartureAt(): ?\DateTimeInterface
+    {
+        return $this->departure_at;
+    }
+
+    public function setDepartureAt(\DateTimeInterface $departure_at): self
+    {
+        $this->departure_at = $departure_at;
+
+        return $this;
+    }
+}
--- src/Infrastructure/Repository/Booking/BookingRepository.php
+++ src/Infrastructure/Repository/Booking/BookingRepository.php
@@ -2,27 +2,65 @@
 
 namespace Infrastructure\Repository\Booking;
 
-use Application\Booking\Transformer\BookingTransformerInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Domain\Business\Booking\Model\Booking as DomainBooking;
-use Domain\Business\Booking\Model\Properties\BookingId;
-use Domain\Business\Booking\Repository\BookingRepositoryInterface;
 use Infrastructure\Entity\Booking\Booking;
 
-class BookingRepository extends ServiceEntityRepository implements BookingRepositoryInterface
+/**
+ * @extends ServiceEntityRepository<Booking>
+ *
+ * @method Booking|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Booking|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Booking[]    findAll()
+ * @method Booking[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BookingRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, private BookingTransformerInterface $bookingTransformer)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Booking::class);
     }
 
-
-    public function save(DomainBooking $domainBooking): BookingId
+    public function add(Booking $entity, bool $flush = false): void
     {
-        $booking = $this->bookingTransformer->fromDomainToDb($domainBooking);
-        //$this->_em->persist($booking);
+        $this->getEntityManager()->persist($entity);
 
-        return $domainBooking->getUuid();
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
     }
+
+    public function remove(Booking $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Booking[] Returns an array of Booking objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('b.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Booking
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
