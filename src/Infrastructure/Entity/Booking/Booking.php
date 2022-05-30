9a5d79be1f63a2ab8bd058d21bc8cd963aa67373
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-05-30 14:37:41 +0300

    add stampable

--- src/Infrastructure/Entity/Booking/Booking.php
+++ src/Infrastructure/Entity/Booking/Booking.php
@@ -6,12 +6,14 @@ namespace Infrastructure\Entity\Booking;
 use Infrastructure\Entity\Uuidable;
 use Infrastructure\Repository\Booking\BookingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Infrastructure\Entity\Timestampable;
 
 #[ORM\Table]
 #[ORM\Entity(repositoryClass: BookingRepository::class)]
 class Booking
 {
     use Uuidable;
+    use Timestampable;
 
     #[ORM\Column(name: 'id', type: 'integer')]
     #[ORM\Id]
