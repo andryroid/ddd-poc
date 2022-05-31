ffbd06066e3152e0be6de3016252dfe924cb0316
Andry Lalaina RATSIMISETA <ratsimisetata@gmail.com>
2022-05-31 09:02:09 +0300

    update code

--- src/Infrastructure/Service/Transformer/Booking/BookingTransformer.php
+++ src/Infrastructure/Service/Transformer/Booking/BookingTransformer.php
@@ -3,13 +3,12 @@
 namespace Infrastructure\Service\Transformer\Booking;
 
 use Application\Booking\Transformer\BookingTransformerInterface;
-use Domain\Utils\Entity\EntityInterface;
 use Domain\Business\Booking\Model\Booking as DomainBooking;
 use Infrastructure\Entity\Booking\Booking as EntityBooking;
 
 final class BookingTransformer implements BookingTransformerInterface 
 {
-    public function fromDomainToDb(DomainBooking $domainBooking): EntityInterface 
+    public function fromDomainToDb(DomainBooking $domainBooking): EntityBooking 
     {
         $dataFromDomain  = $domainBooking->getSummary();
         return (new EntityBooking())
