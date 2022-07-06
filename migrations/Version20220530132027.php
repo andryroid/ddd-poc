2972c7a6e9192bfa848293409a8b74b1b068d93b
Andry Lalaina <lalaina@wylog.com>
2022-07-06 17:06:00 +0300

    setup dev docker

--- migrations/Version20220530132027.php
+++ migrations/Version20220530132027.php
@@ -1,6 +1,7 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1)
+;
 
 namespace DoctrineMigrations;
 
@@ -20,7 +21,7 @@ final class Version20220530132027 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, uuid VARCHAR(255) NOT NULL, person JSON NOT NULL, contacts JSON NOT NULL, departure VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL, departure_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE booking (id INT auto_increment NOT NULL primary key, uuid VARCHAR(255) NOT NULL, person JSON NOT NULL, contacts JSON NOT NULL, departure VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL, departure_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
